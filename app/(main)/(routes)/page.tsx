be8f60ed2c63d37c86d5a2ca59de623da844cdc7
Eranmonnie <ajalaferanmi123@gmail.com>
2024-03-30 16:40:36 +0100

    initial setup shadcn tailwind next eslint clerk

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton
+      afterSignOutUrl="/"
+      />
+      this is a protected route
+    </div>
+  );
+}
